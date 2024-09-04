3e60b4b5c4bdc5b63cc835575e24eeba09520aa8
Shirin-Torkamandi <shirin.t44@gmail.com>
2024-09-04 10:28:02 +0330

    delete css line and add Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 // import data from './assets/data.json'
 // import heroBg from '/hero-bg.jpg'
-import "./App.css";
 // import something from ./components/something.jsx
+import Header from "./components/Header.jsx";
 
 function App() {
   return (
     <>
       <div>{/* <img src={heroBg} className="logo" alt="Vite logo" /> */}</div>
+      <Header />
     </>
   );
 }
